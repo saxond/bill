813f8dcd51e18b6c0b75532d87453560376c0f5e
Saxon D'Aubin <saxond@mac.com>
2023-12-30 00:12:43 -0800

    Code to generate sql inserts

--- server/src/test/java/com/bill/server/ContactRepositoryTest.java
+++ server/src/test/java/com/bill/server/ContactRepositoryTest.java
@@ -19,4 +19,31 @@ class ContactRepositoryTest {
         var all = contactRepository.getAll();
         assertNotNull(contactRepository.get(all.get(0).id()));
     }
+
+    @Test
+    void insert() {
+        var all = new ContactRepository().getAll();
+        all.forEach(contact -> {
+            var builder = new StringBuilder();
+            builder.append("INSERT into contacts(id, avatar, first, last, twitter) VALUES (");
+            builder.append(quote(contact.id()))
+                    .append(',')
+                    .append(quote(contact.avatar()))
+                    .append(',')
+                    .append(quote(contact.first()))
+                    .append(',')
+                    .append(quote(contact.last()))
+                    .append(',')
+                    .append(quote(contact.twitter()))
+                    .append(");");
+            System.out.println(builder.toString());
+        });
+    }
+
+    static String quote(String value) {
+        if (value == null) {
+            return "null";
+        }
+        return '"' + value + '"';
+    }
 }
