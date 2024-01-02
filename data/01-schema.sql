e6a28d36fe2643b71222166c5da4be34a22d8bfe
Saxon D'Aubin <saxond@mac.com>
2024-01-01 16:56:57 -0800

    Define contacts primary key

--- data/01-schema.sql
+++ data/01-schema.sql
@@ -1,9 +1,10 @@
 
-create table contacts(
-    id varchar(80),
+CREATE TABLE contacts(
+    id varchar(80) NOT NULL,
     avatar varchar(255),
-    first varchar(255),
-    last varchar(255),
+    first varchar(255) NOT NULL,
+    last varchar(255) NOT NULL,
     twitter varchar(255),
-    notes varchar(1024)
+    notes varchar(1024),
+    PRIMARY KEY (id)
 );
