e9251f51bafc75579226751911963e21bf899a9a
Saxon D'Aubin <saxond@mac.com>
2024-01-02 08:41:50 -0800

    Update contact.edit.tsx

--- app/src/routes/contact.edit.tsx
+++ app/src/routes/contact.edit.tsx
@@ -52,56 +52,58 @@ export default function EditContact(props: Props) {
   if (loading) return (<div>Loading</div>);
 
   return (
-    <form id="contact-form" method="post" onSubmit={onSubmit}>
-      <p>
-        <span>Name</span>
-        <input
-          defaultValue={contact.first}
-          aria-label="First name"
-          name="first"
-          type="text"
-          placeholder="First"
-        />
-        <input
-          aria-label="Last name"
-          defaultValue={contact.last}
-          name="last"
-          placeholder="Last"
-          type="text"
-        />
-      </p>
-      <label>
-        <span>Twitter</span>
-        <input
-          defaultValue={contact.twitter}
-          name="twitter"
-          placeholder="@jack"
-          type="text"
-        />
-      </label>
-      <label>
-        <span>Avatar URL</span>
-        <input
-          aria-label="Avatar URL"
-          defaultValue={contact.avatar}
-          name="avatar"
-          placeholder="https://example.com/avatar.jpg"
-          type="text"
-        />
-      </label>
-      <label>
-        <span>Notes</span>
-        <textarea
-          defaultValue={contact.notes}
-          name="notes"
-          rows={6}
-        />
-      </label>
-      <p>
-        <button type="submit">{contact?.id ? `Save` : `Create`}</button>
-        <button type="button">Cancel</button>
-      </p>
+    <>
+      <form id="contact-form" method="post" onSubmit={onSubmit}>
+        <p>
+          <span>Name</span>
+          <input
+            defaultValue={contact.first}
+            aria-label="First name"
+            name="first"
+            type="text"
+            placeholder="First"
+          />
+          <input
+            aria-label="Last name"
+            defaultValue={contact.last}
+            name="last"
+            placeholder="Last"
+            type="text"
+          />
+        </p>
+        <label>
+          <span>Twitter</span>
+          <input
+            defaultValue={contact.twitter}
+            name="twitter"
+            placeholder="@jack"
+            type="text"
+          />
+        </label>
+        <label>
+          <span>Avatar URL</span>
+          <input
+            aria-label="Avatar URL"
+            defaultValue={contact.avatar}
+            name="avatar"
+            placeholder="https://example.com/avatar.jpg"
+            type="text"
+          />
+        </label>
+        <label>
+          <span>Notes</span>
+          <textarea
+            defaultValue={contact.notes}
+            name="notes"
+            rows={6}
+          />
+        </label>
+        <p>
+          <button type="submit">{contact?.id ? `Save` : `Create`}</button>
+          <button type="button">Cancel</button>
+        </p>
+      </form>
       { updated && <Link to={`/contacts/${contact.id}`}>Updated</Link> }
-    </form>
+    </>
   );
 }
